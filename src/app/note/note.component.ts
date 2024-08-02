0743db3f53dc92822ad8a3a238a4b50cc8a3dacf
asp <asp@aptech.com>
2024-08-02 13:07:00 +0500

    first commit

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-note',
+  selector: 'app-notes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule, NgbModal],
   templateUrl: './note.component.html',
-  styleUrl: './note.component.css'
+  styleUrls: ['./note.component.css']
 })
-export class NoteComponent {
+export class NotesComponent implements OnInit {
+  notes: {
+    key: string; id: number; title: string; description: string; 
+  }[] = [];
+  nextId = 1;
+  currentNote: { title: string; description: string; key?: string } = { title: '', description: '' };
 
+  constructor(private http: HttpClient, private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+    this.loadNotes();
+  }
+
+  loadNotes(): void {
+    this.http.get<{ [key: string]: { title: string; description: string; } }>('https://notes-386f7-default-rtdb.firebaseio.com/notes.json')
+      .subscribe(data => {
+        this.notes = Object.keys(data).map(key => ({ id: this.nextId++, key, ...data[key] }));
+      });
+  }
+
+  openAddNoteModal(content: any): void {
+    this.currentNote = { title: '', description: '' };
+    this.modalService.open(content);
+  }
+
+  openEditNoteModal(content: any, note: any): void {
+    this.currentNote = { ...note };
+    this.modalService.open(content);
+  }
+
+  addNote(): void {
+    const newNote = { title: this.currentNote.title, description: this.currentNote.description };
+    this.http.post<{ name: string }>('https://notes-386f7-default-rtdb.firebaseio.com/notes.json', newNote)
+      .subscribe(res => {
+        this.currentNote.key = res.name;
+        this.notes.push({ ...newNote, id: this.nextId++, key: res.name });
+        this.modalService.dismissAll();
+      });
+  }
+
+  updateNote(): void {
+    const { key, ...note } = this.currentNote;
+    if (key) {
+      this.http.put(`https://notes-386f7-default-rtdb.firebaseio.com/notes/${key}.json`, note)
+        .subscribe(() => {
+          const index = this.notes.findIndex(n => n.key === key);
+          if (index !== -1) {
+            this.notes[index] = { ...note, id: this.notes[index].id, key };
+          }
+          this.modalService.dismissAll();
+        });
+    } else {
+      console.error('Note key is missing');
+    }
+  }
+  
+
+  deleteNote(key: string): void {
+    this.http.delete(`https://notes-386f7-default-rtdb.firebaseio.com/notes/${key}.json`)
+      .subscribe(() => {
+        this.notes = this.notes.filter(note => note.key !== key);
+      });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-export const environment = {
-    firebaseConfig : {
-        apiKey: "AIzaSyDvgSjTKLjj38pVb14kBrE8NYZRAtr48B8",
-        authDomain: "crud-73a51.firebaseapp.com",
-        projectId: "crud-73a51",
-        storageBucket: "crud-73a51.appspot.com",
-        messagingSenderId: "1030010888475",
-        appId: "1:1030010888475:web:3cb9f9b13c978c78e1b3be"
-      },
-      production: false
-};
+
